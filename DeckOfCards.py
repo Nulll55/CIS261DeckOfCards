32b40a9ecf05e9d64192c757715ffa41608de138
username <useremail>
2025-05-28 16:50:56 -0700

    Add project files.

---
+++ DeckOfCards.py
@@ -0,0 +1,31 @@
+# Emma Kailani Tirado
+# CIS261 Deck Of Cards week 8.2 lab
+# 05/28/2025
+
+import random
+
+class Card:
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+
+class Deck:
+    def __init__(self):
+        self.deck = []
+
+
+        ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+        suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+# def shuffle
+# def deal
+
+
+# def main
+
+    #print("No more cards left in the deck.")
+    #print("Game ended.")
+
+if __name__ == "__main__":
+    main()
